eadb77d749124dfd9246db405417981dce3a105b
dugweb <dugweb@gmail.com>
2015-08-30 16:18:36 -0600

    added database create sql

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,24 @@
 from flask import Flask
+from dbconnect import connection
 
 app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Moneyky, Hello World"
+
+
+@app.route("/database")
+def database():
+
+	try:
+		c, conn = connection()
+		return "database page"
+	except Exception as e:
+		return (str(e))
+
+
+
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
